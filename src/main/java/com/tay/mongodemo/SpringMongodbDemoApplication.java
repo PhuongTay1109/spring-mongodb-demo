fcec7f76982b613adfbbed1e153a7f2afefd1a68
tay <phuonggteyy@gmail.com>
2024-06-05 10:45:57 +0700

    init project

---
+++ src/main/java/com/tay/mongodemo/SpringMongodbDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tay.mongodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMongodbDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMongodbDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tay/mongodemo/SpringMongodbDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tay.mongodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMongodbDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
